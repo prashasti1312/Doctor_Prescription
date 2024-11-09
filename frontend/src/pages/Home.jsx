e71e8c468570dd9b4721e67a3107ba364df77e7c
prashasti1312 <vyasprashasti13@gmail.com>
2024-11-09 18:07:36 +0530

    initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Header></Header>
+      <SpecialityMenu></SpecialityMenu>
+      <TopDoctors></TopDoctors>
+      <Banner></Banner>
+      {/* <Footer></Footer> */}
+    </div>
+  )
+}
+
+export default Home
